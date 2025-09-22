4756c24e827422183248636549c249e6f4717f1b
Safidy RAMAROSON <safidyramaroson.patrick@gmail.com>
2025-09-23 00:04:08 +0300

    feat: add footer copyright component

---
+++ components/atoms/FooterCopyright.tsx
@@ -0,0 +1,10 @@
+
+export function FooterCopyRight() {
+    return (
+        <div className="py-8 bg-secondary">
+            <p className="text-center text-xl text-primary-foreground font-bold mb-2">
+                © Tous droits réservés à Blu Azur
+            </p>
+        </div>
+    )
+}
