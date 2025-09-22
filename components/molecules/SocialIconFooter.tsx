8c170aaec634bc4d607aea782282c8b85f8ebe65
Safidy RAMAROSON <safidyramaroson.patrick@gmail.com>
2025-09-23 00:04:36 +0300

    feat: add social icon footer component

---
+++ components/molecules/SocialIconFooter.tsx
@@ -0,0 +1,29 @@
+import { cn } from "@/lib/utils"
+import { LucideProps } from "lucide-react"
+import Link from "next/link"
+import { ForwardRefExoticComponent, RefAttributes } from "react"
+
+export function SocialIconFooter({
+    icon: Icon,
+    href,
+    label,
+    target = "_blank",
+    className,
+}: {
+    icon: ForwardRefExoticComponent<Omit<LucideProps, "ref"> & RefAttributes<SVGSVGElement>>,
+    href: string,
+    label: string,
+    target?: string,
+    className?: string,
+}) {
+    return (
+        <span className={cn('text-base flex flex-row items-center gap-4', className)}>
+            <span className="border border-white rounded-full p-1 bg-primary-foreground">
+                <Icon className="size-5 text-primary" />
+            </span>
+            <Link href={href} target={target} className="text-[18px]">
+                {label}
+            </Link>
+        </span>
+    )
+}
