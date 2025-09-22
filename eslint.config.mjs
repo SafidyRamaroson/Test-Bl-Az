358a8581e12839d3a188eb8cee8ae67d1cb9d854
Safidy RAMAROSON <safidyramaroson.patrick@gmail.com>
2025-09-22 22:03:54 +0300

    "feat: Add ESLint plugins for import, jsx-a11y, react, react-hooks"

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,21 +5,61 @@ import { FlatCompat } from "@eslint/eslintrc";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+const compat = new FlatCompat({ baseDirectory: __dirname });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+// ESLint flat config
+export default [
+  ...compat.extends(
+    "next/core-web-vitals",
+    "next/typescript",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:jsx-a11y/recommended"
+  ),
   {
     ignores: [
-      "node_modules/**",
+      "**/node_modules/**",
       ".next/**",
       "out/**",
       "build/**",
       "next-env.d.ts",
-    ],
+      "eslint.config.mjs",
+      "postcss.config.mjs"
+    ]
   },
-];
+  {
+    languageOptions: {
+      parserOptions: {
+        project: "./tsconfig.json",
+        tsconfigRootDir: __dirname
+      }
+    }
+  },
+  {
+    rules: {
+      // Général
+      "no-console": "off",
 
-export default eslintConfig;
+      // Typescript
+      "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+
+      // React
+      "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off",
+      "react/jsx-filename-extension": ["warn", { extensions: [".tsx", ".jsx"] }],
+      "react/jsx-props-no-spreading": "off",
+
+      // Imports
+      "import/prefer-default-export": "off",
+      "import/extensions": [
+        "error",
+        "ignorePackages",
+        {
+          ts: "never",
+          tsx: "never",
+          js: "never",
+          jsx: "never"
+        }
+      ]
+    }
+  }
+];
