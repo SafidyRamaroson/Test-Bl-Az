76019c3064c0f2868bfecdad0c34c500a7288052
Safidy RAMAROSON <safidyramaroson.patrick@gmail.com>
2025-09-23 00:05:07 +0300

    feat: Add Blu Azur contacts data

---
+++ data/bluAzurContacts.ts
@@ -0,0 +1,21 @@
+import { Facebook, Mail, Phone } from "lucide-react"
+
+const bluAzurContacts = [
+    {
+        icon: Mail,
+        href: "info@bluazur.com",
+        label: "info@bluazur.com"
+    },
+    {
+        icon: Phone,
+        href: "+1 555 555-5555",
+        label: "+1 555 555-5555"
+    },
+    {
+        icon: Facebook,
+        href: "@ BLU Azur",
+        label: "@ BLU Azur"
+    }
+]
+
+export default bluAzurContacts;
