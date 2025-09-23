fc5d131319a111ea71d83506ada2cecf4e1ce093
Safidy RAMAROSON <safidyramaroson.patrick@gmail.com>
2025-09-23 09:20:03 +0300

    feat: add SaintRaphaelSection

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import { FooterSection } from "@/components/templates/FooterSection";
+import { SaintRaphaelSection } from "@/components/templates/SaintRaphaelSection";
 
 export default function Home() {
   return (
     <div>
+      <SaintRaphaelSection />
       <FooterSection />
     </div>
   );
