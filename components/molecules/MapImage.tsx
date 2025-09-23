e290b5ce660e85459ede7351feef3fa25531b722
Safidy RAMAROSON <safidyramaroson.patrick@gmail.com>
2025-09-23 15:02:14 +0300

    feat: Add MapImage component

---
+++ components/molecules/MapImage.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+
+
+export function MapImage() {
+    return (
+        <div className="relative flex justify-center">
+            <Image
+                src="/blu-azur-map.png"
+                alt="Blu Azur map"
+                width={1026}
+                height={1225}
+                className="w-[441px] h-[526px] rounded-4xl"
+            />
+            <Image
+                src="/map.png"
+                alt="Mini map"
+                width={94}
+                height={82}
+                className="absolute -top-10 -left-12"
+            />
+        </div>
+    );
+}
