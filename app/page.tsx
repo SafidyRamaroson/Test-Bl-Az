cc255e8c03313a36ccd040a7e06911240935c1c0
Safidy RAMAROSON <safidyramaroson.patrick@gmail.com>
2025-09-23 13:24:09 +0300

    "feat: Add ExploreBluAzurSection to Home page"

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { ExploreBluAzurSection } from "@/components/templates/ExploreBluAzurSection";
 import { FooterSection } from "@/components/templates/FooterSection";
 import { ReserveNowSection } from "@/components/templates/ReserveNowSection";
 import { SaintRaphaelSection } from "@/components/templates/SaintRaphaelSection";
@@ -6,6 +7,7 @@ export default function Home() {
   return (
     <div>
       <SaintRaphaelSection />
+      <ExploreBluAzurSection />
       <ReserveNowSection />
       <FooterSection />
     </div>
