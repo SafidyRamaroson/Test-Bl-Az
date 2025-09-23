70c06a9db3aabe7907e2f425b79493ffde35d3d2
Safidy RAMAROSON <safidyramaroson.patrick@gmail.com>
2025-09-23 09:17:07 +0300

    feat: add Saint Raphael services data

---
+++ data/saintRaphaelServices.ts
@@ -0,0 +1,34 @@
+
+
+export const saintRaphaelServices = [
+    {
+        src: '/swimming.png',
+        alt: 'swimming-Saint-Raphael',
+        name: 'Sports nautiques,  plongée, voile'
+    },
+    {
+        src: '/city.png',
+        alt: 'city-Saint-Raphael',
+        name: 'Randonnées et vélo dans l’Estérel'
+    },
+    {
+        src: '/casino.png',
+        alt: 'casino-Saint-Raphael',
+        name: 'Casino et vie nocturne animée'
+    },
+    {
+        src: '/village.png',
+        alt: 'village-Saint-Raphael',
+        name: 'Villages perchés et  marchés provençaux'
+    },
+    {
+        src: '/golf.png',
+        alt: 'golf-Saint-Raphael',
+        name: 'Un paradis pour les golfeurs'
+    },
+    {
+        src: '/mountains.png',
+        alt: 'mountains-Saint-Raphael',
+        name: 'Montagne et vélo de route'
+    }
+]
