b7efca8a84b6800dd05534334d53a594c35b46e9
Safidy RAMAROSON <safidyramaroson.patrick@gmail.com>
2025-09-23 15:03:23 +0300

    feat: add BluAzur location section with map

---
+++ components/templates/BluAzurLocalisationSection.tsx
@@ -0,0 +1,26 @@
+import { Button } from "../ui/button";
+import { CircleArrowRight } from "lucide-react";
+import { MapImage } from "../molecules/MapImage";
+
+
+export function BluAzurLocalisationSection() {
+    return (
+        <div className="bg-[url('/blu-azur-localisation-background.png')] min-h-screen bg-cover bg-center py-24">
+            {/* wrapper */}
+            <div className="w-3/4 mx-auto grid grid-cols-2 gap-[234px] justify-center items-center">
+                {/* Blu azur localisation */}
+                <div className="text-justify text-[#121212] mb-[21px]">
+                    <h2 className="text-[34px] font-normal">Où se trouve Blu Azur à <br /> <span className="font-bold">Saint-Raphaël ?</span> </h2>
+                    <p className="text-[#020202] text-sm font-bold my-[21px]">Blu Azur est idéalement situé entre Saint-Tropez et Cannes, au cœur de l’une des plus belles stations balnéaires de France. Nos appartements se trouvent dans un domaine privé sécurisé, entouré de verdure, à quelques minutes à pied de la plage sablonneuse de Santa Lucia et de la marina. Proche du centre-ville de Saint-Raphaël, la résidence permet un accès facile aux boutiques, restaurants et animations locales. Une station d’arrêt d’autobus est située à la sortie du site, facilitant les déplacements, et une boulangerie à proximité vous offre du pain frais chaque matin pour bien commencer la journée.</p>
+                    {/* how to get there */}
+                    <Button size='lg' className="border border-primary text-primary hover:text-primary-foreground hover:border-primary-foreground cursor-pointer bg-white rounded-full">
+                        Comment s&apos;y rendre
+                        <CircleArrowRight className="text-primary-foreground size-4 ml-2" />
+                    </Button>
+                </div>
+                {/* Blu azur map */}
+                <MapImage />
+            </div>
+        </div>
+    )
+}
