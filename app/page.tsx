28f9dd0b5097a4763daba0e2a6063bd2995f69d9
Safidy RAMAROSON <safidyramaroson.patrick@gmail.com>
2025-09-23 09:58:30 +0300

    feat: add reserve now section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import { FooterSection } from "@/components/templates/FooterSection";
+import { ReserveNowSection } from "@/components/templates/ReserveNowSection";
 import { SaintRaphaelSection } from "@/components/templates/SaintRaphaelSection";
 
 export default function Home() {
   return (
     <div>
       <SaintRaphaelSection />
+      <ReserveNowSection />
       <FooterSection />
     </div>
   );
---
+++ components/templates/ReserveNowSection.tsx
@@ -0,0 +1,16 @@
+import { ReserveNowButton } from "../atoms/ReserveNowButton";
+import { ReserveNowHeader } from "../molecules/ReserveNowHeader";
+
+
+export function ReserveNowSection() {
+    return (
+        <div className="relative bg-[url('/residence-pierre-vacances-la-corniche-d-or.png')] bg-cover bg-center py-12 min-h-screen flex flex-col items-center justify-center z-10">
+            {/* overlay */}
+            <div className="absolute inset-0 bg-linear-to-r from-primary to-primary/10 h-full w-full -z-10"></div>
+
+            {/* Content */}
+            <ReserveNowHeader />
+            <ReserveNowButton />
+        </div>
+    )
+}
