35ffec607822265c106ee54611713faac76c0077
Safidy RAMAROSON <safidyramaroson.patrick@gmail.com>
2025-09-23 19:25:32 +0300

    feat: Add className prop to ReserveNowButton

--- components/atoms/ReserveNowButton.tsx
+++ components/atoms/ReserveNowButton.tsx
@@ -1,11 +1,14 @@
 import Link from "next/link";
 import { Button } from "../ui/button";
 import { CircleArrowRight } from "lucide-react";
+import { cn } from "@/lib/utils";
 
 
-export function ReserveNowButton() {
+export function ReserveNowButton({
+    className
+}: { className?: string }) {
     return (
-        <Link href='#' className="cursor-pointer z-10">
+        <Link href='#' className={cn("cursor-pointer z-10", className)}>
             <Button size="lg" className="rounded-full py-4 px-2 text-[16px] font-[900]">
                 Réservez dès maintenant votre séjour !
                 <CircleArrowRight className="text-primary-foreground size-4 ml-2" />
