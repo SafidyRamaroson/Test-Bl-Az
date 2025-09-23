749e8b73fbc194fbf52514c252285dd1c23ed44d
Safidy RAMAROSON <safidyramaroson.patrick@gmail.com>
2025-09-23 13:29:32 +0300

    fix: remove unnecessary border styling

--- components/templates/ExploreBluAzurSection.tsx
+++ components/templates/ExploreBluAzurSection.tsx
@@ -4,7 +4,7 @@ import { ActivitiesList } from "../organims/ActivitiesList";
 export function ExploreBluAzurSection() {
     return (
         <section className="bg-[url('/pic-de-lourse.png')] bg-cover bg-no-repeat bg-center">
-            <div className="w-3/4 mx-auto z-10 border border-red-300 py-[75px]">
+            <div className="w-3/4 mx-auto z-10 py-[75px]">
                 {/* Activities List */}
                 <ActivitiesList />
             </div>
