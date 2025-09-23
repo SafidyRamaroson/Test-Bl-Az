6fc2762c16e6c94c6f7fb0635e49df9033adbc49
Safidy RAMAROSON <safidyramaroson.patrick@gmail.com>
2025-09-23 10:02:43 +0300

    fix: adjust background image scaling

--- components/templates/ReserveNowSection.tsx
+++ components/templates/ReserveNowSection.tsx
@@ -4,7 +4,7 @@ import { ReserveNowHeader } from "../molecules/ReserveNowHeader";
 
 export function ReserveNowSection() {
     return (
-        <div className="relative bg-[url('/residence-pierre-vacances-la-corniche-d-or.png')] bg-cover bg-center py-12 min-h-screen flex flex-col items-center justify-center z-10">
+        <div className="relative bg-[url('/residence-pierre-vacances-la-corniche-d-or.png')] bg-contain py-12 min-h-screen flex flex-col items-center justify-center z-10">
             {/* overlay */}
             <div className="absolute inset-0 bg-linear-to-r from-primary to-primary/10 h-full w-full -z-10"></div>
 
