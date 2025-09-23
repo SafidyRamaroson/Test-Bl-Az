508a2279869438bd0e064810bc4ad928475e3718
Safidy RAMAROSON <safidyramaroson.patrick@gmail.com>
2025-09-23 13:23:58 +0300

    feat: add ExploreBluAzurSection component

---
+++ components/templates/ExploreBluAzurSection.tsx
@@ -0,0 +1,13 @@
+import { ActivitiesList } from "../organims/ActivitiesList";
+
+
+export function ExploreBluAzurSection() {
+    return (
+        <section className="bg-[url('/pic-de-lourse.png')] bg-cover bg-no-repeat bg-center">
+            <div className="w-3/4 mx-auto z-10 border border-red-300 py-[75px]">
+                {/* Activities List */}
+                <ActivitiesList />
+            </div>
+        </section>
+    )
+}
