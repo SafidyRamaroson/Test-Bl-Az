58982de57ba3e173870f22c3c252351219522fe6
Safidy RAMAROSON <safidyramaroson.patrick@gmail.com>
2025-09-24 14:50:38 +0300

    feat: Update ReserveNowButton and ReserveNowHeader for better readability

--- components/molecules/ReserveNowHeader.tsx
+++ components/molecules/ReserveNowHeader.tsx
@@ -4,7 +4,7 @@ export function ReserveNowHeader() {
     return (
         <div className="text-center w-full">
             <h2 className="font-bold text-[42px] text-primary-foreground">Réservations</h2>
-            <p className="text-[21px] font-normal text-primary-foreground py-8"> <span className="font-bold">Offrez-vous un séjour inoubliable</span> dans l’un de nos appartements Blu <br /> Azur. Profitez d’un cadre exceptionnel, entre confort, détente et <br /> découvertes, au cœur de Saint-Raphaël.</p>
+            <p className="text-[21px] font-normal text-primary-foreground py-8"> <span className="font-bold">Offrez-vous un séjour inoubliable</span> dans l’un de nos appartements Blu <br className="max-md:hidden" /> Azur. Profitez d’un cadre exceptionnel, entre confort, détente et <br className="max-md:hidden" /> découvertes, au cœur de Saint-Raphaël.</p>
         </div>
     )
 }
