4cf005c62a04eb6392cf73f6486a6d41d844f694
Safidy RAMAROSON <safidyramaroson.patrick@gmail.com>
2025-09-24 14:35:18 +0300

    fix: adjust map and layout responsiveness

--- components/molecules/MapImage.tsx
+++ components/molecules/MapImage.tsx
@@ -3,22 +3,22 @@ import Image from "next/image";
 
 export function MapImage() {
     return (
-        <div className="relative flex justify-center">
-            <div className="relative w-[441px] h-[526px]">
+        <div className="flex justify-center">
+            <div className="relative w-[441px] h-[441px] md:h-[526px]">
                 <Image
                     src="/blu-azur-map.avif"
                     alt="Blu Azur map"
                     fill
                     className="object-cover rounded-4xl"
                 />
+                <Image
+                    src="/map.png"
+                    alt="Mini map"
+                    width={94}
+                    height={82}
+                    className="absolute -top-10 -left-12"
+                />
             </div>
-            <Image
-                src="/map.png"
-                alt="Mini map"
-                width={94}
-                height={82}
-                className="absolute -top-10 -left-12"
-            />
         </div>
     );
 }
--- components/templates/BluAzurLocalisationSection.tsx
+++ components/templates/BluAzurLocalisationSection.tsx
@@ -5,12 +5,12 @@ import { MapImage } from "../molecules/MapImage";
 
 export function BluAzurLocalisationSection() {
     return (
-        <div className="bg-[url('/blu-azur-localisation-background.webp')] min-h-screen bg-cover bg-center py-24">
+        <div className="bg-[url('/blu-azur-localisation-background.webp')] min-h-screen bg-cover bg-center py-8  md:py-12 lg:py-24">
             {/* wrapper */}
-            <div className="w-3/4 mx-auto grid grid-cols-2 gap-[234px] justify-center items-center">
+            <div className="w-4/5 md:w-3/4 mx-auto grid grid-cols-1 gap-x-[20px] md:grid-cols-2 md:gap-[100px] lg:gap-[234px] justify-center items-center">
                 {/* Blu azur localisation */}
                 <div className="text-justify text-[#121212] mb-[21px]">
-                    <h2 className="text-[34px] font-normal">Où se trouve Blu Azur à <br /> <span className="font-bold">Saint-Raphaël ?</span> </h2>
+                    <h2 className="text-[34px] font-normal">Où se trouve Blu Azur à <br className="max-md:hidden" /> <span className="font-bold">Saint-Raphaël ?</span> </h2>
                     <p className="text-[#020202] text-sm font-bold my-[21px]">Blu Azur est idéalement situé entre Saint-Tropez et Cannes, au cœur de l’une des plus belles stations balnéaires de France. Nos appartements se trouvent dans un domaine privé sécurisé, entouré de verdure, à quelques minutes à pied de la plage sablonneuse de Santa Lucia et de la marina. Proche du centre-ville de Saint-Raphaël, la résidence permet un accès facile aux boutiques, restaurants et animations locales. Une station d’arrêt d’autobus est située à la sortie du site, facilitant les déplacements, et une boulangerie à proximité vous offre du pain frais chaque matin pour bien commencer la journée.</p>
                     {/* how to get there */}
                     <Button size='lg' className="border border-primary text-primary hover:text-primary-foreground hover:border-primary-foreground cursor-pointer bg-white/80 rounded-full">
