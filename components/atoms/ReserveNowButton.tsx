f6f095e4493c6c5b077ca500f0d687ccd5398ff2
Safidy RAMAROSON <safidyramaroson.patrick@gmail.com>
2025-09-24 21:09:36 +0300

    fix: adjust ReserveNowButton styling and structure

--- components/atoms/ReserveNowButton.tsx
+++ components/atoms/ReserveNowButton.tsx
@@ -3,16 +3,15 @@ import { Button } from "../ui/button";
 import { CircleArrowRight } from "lucide-react";
 import { cn } from "@/lib/utils";
 
-
-export function ReserveNowButton({
-    className
-}: { className?: string }) {
+export function ReserveNowButton({ className }: { className?: string }) {
     return (
-        <Link href='#' className={cn("cursor-pointer z-10", className)}>
-            <Button size="lg" className="rounded-full text-white cursor-pointer py-4 px-2 text-xs sm:text-base text-[16px] font-[900]">
+        <Link href="#" className={cn("cursor-pointer z-10", className)}>
+            <Button
+                size="lg"
+                className="rounded-full text-white cursor-pointer py-3 px-4 text-sm sm:text-base md:text-lg font-extrabold flex items-center">
                 Réservez dès maintenant votre séjour !
-                <CircleArrowRight className="text-primary-foreground size-4 ml-2" />
+                <CircleArrowRight className="text-primary-foreground w-4 h-4 ml-2 flex-shrink-0" />
             </Button>
         </Link>
-    )
+    );
 }
