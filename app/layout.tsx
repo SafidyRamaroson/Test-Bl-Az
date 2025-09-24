547b793fef9fdb25da47bf5b1be6caeb8fdf4b85
Safidy RAMAROSON <safidyramaroson.patrick@gmail.com>
2025-09-24 10:41:51 +0300

    fix: replace geist with mulish font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,15 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Mulish } from "next/font/google";
 import './styles/globals.css';
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const mulish = Mulish({
+  variable: "--font-mulish",
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: ["400", "500", "700", "900"]
 });
 
 export const metadata: Metadata = {
-  title: "Accueil | Blu d&apos;azur",
+  title: "Accueil | Blu d\'azur",
   description: "Blu Azur vous invite à découvrir le charme de Saint-Raphaël, une destination d’exception nichée entre Cannes et Saint-Tropez. Nos appartements tout équipés, situés dans un domaine privé sécurisé, offrent un cadre verdoyant et paisible, à quelques pas de la marina de Santa Lucia, des plages de sable fin et du centre-ville animé. Profitez d’un séjour alliant confort, sérénité et élégance au cœur de la Riviera française !",
 };
 
@@ -25,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${mulish.variable}  antialiased`}
       >
         {children}
       </body>
