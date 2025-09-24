8585fa09f1d197ba9e3e4edf35b21e8b9f125e7a
Safidy RAMAROSON <safidyramaroson.patrick@gmail.com>
2025-09-24 09:16:06 +0300

    fix: add aria-label to social links

--- components/templates/HeroSection.tsx
+++ components/templates/HeroSection.tsx
@@ -13,16 +13,16 @@ export function HeroSection() {
 
             {/* Aside social links */}
             <div className="w-[75px] h-full absolute top-0 left-0 bg-[#4097FF]/60 flex flex-col justify-end items-center gap-y-6 pb-[87px]">
-                <Link href='#' target="_blank">
+                <Link href='#' target="_blank" aria-label="facebook">
                     <FacebookIcon className="text-white size-6 mx-auto" />
                 </Link>
-                <Link href='#' target="_blank">
+                <Link href='#' target="_blank" aria-label="youtube">
                     <Youtube className="text-white size-6 mx-auto !opacity-100" />
                 </Link>
-                <Link href='#' target="_blank">
+                <Link href='#' target="_blank" aria-label="twitter">
                     <TwitterIcon className="text-white size-6 mx-auto" />
                 </Link>
-                <Link href='#' target="_blank">
+                <Link href='#' target="_blank" aria-label="instagram">
                     <InstagramIcon className="text-white size-6 mx-auto" />
                 </Link>
             </div>
