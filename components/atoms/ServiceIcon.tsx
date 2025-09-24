c9b23482f7ea5b45004bec34f9b2beb2009876ca
Safidy RAMAROSON <safidyramaroson.patrick@gmail.com>
2025-09-24 21:11:17 +0300

    fix: adjust ServiceIcon styling and sizing

--- components/atoms/ServiceIcon.tsx
+++ components/atoms/ServiceIcon.tsx
@@ -4,14 +4,20 @@ import Image from "next/image";
 
 export function ServiceIcon({ src, alt, className }: { src: string, alt: string, className?: string }) {
     return (
-        <div className={cn('rounded-full bg-white', className)}>
+        <div
+            className={cn(
+                "rounded-full bg-white flex items-center justify-center",
+                className
+            )}
+        >
             <Image
                 src={src}
                 alt={alt}
                 width={127}
                 height={127}
-                className="object-contain p-8"
+                className="object-contain p-4 w-16 h-16 sm:w-20 sm:h-20 md:w-28 md:h-28 lg:w-32 lg:h-32"
             />
         </div>
+
     )
 }
