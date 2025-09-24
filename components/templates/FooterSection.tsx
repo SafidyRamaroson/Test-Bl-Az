847d43c363b5fa25d46ada0df0f291043aaf3186
Safidy RAMAROSON <safidyramaroson.patrick@gmail.com>
2025-09-24 14:42:07 +0300

    fix: adjust footer layout for mobile

--- components/templates/FooterSection.tsx
+++ components/templates/FooterSection.tsx
@@ -6,8 +6,8 @@ import { FooterCopyRight } from "../atoms/FooterCopyright";
 export function FooterSection() {
     return (
         <section className="bg-primary text-primary-foreground">
-            <div className="flex flex-row justify-center gap-[170px] py-24">
-                <BluAzurLogo />
+            <div className="flex flex-col md:flex-row items-center justify-center gap-[170px] py-24">
+                <BluAzurLogo className="max-md:w-44 max-md:h-44" />
                 <BluAzurContact />
             </div>
             <FooterCopyRight />
