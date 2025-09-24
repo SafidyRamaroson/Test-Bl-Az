6a720377ca84d1f2c514e8ae8d97a9350c46a887
Safidy RAMAROSON <safidyramaroson.patrick@gmail.com>
2025-09-24 09:51:47 +0300

    fix: update MapImage component with responsive image

--- components/molecules/MapImage.tsx
+++ components/molecules/MapImage.tsx
@@ -4,13 +4,14 @@ import Image from "next/image";
 export function MapImage() {
     return (
         <div className="relative flex justify-center">
-            <Image
-                src="/blu-azur-map.png"
-                alt="Blu Azur map"
-                width={875}
-                height={717}
-                className="w-[441px] h-[526px] rounded-4xl"
-            />
+            <div className="relative w-[441px] h-[526px]">
+                <Image
+                    src="/blu-azur-map.png"
+                    alt="Blu Azur map"
+                    fill
+                    className="object-cover rounded-4xl"
+                />
+            </div>
             <Image
                 src="/map.png"
                 alt="Mini map"
