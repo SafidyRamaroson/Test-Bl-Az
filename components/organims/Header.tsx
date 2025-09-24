a899f0d21bd33089abded9e9cebe3c102a84b8c4
Safidy RAMAROSON <safidyramaroson.patrick@gmail.com>
2025-09-24 21:10:36 +0300

    Feat: Widen Header on larger screens

--- components/organims/Header.tsx
+++ components/organims/Header.tsx
@@ -13,7 +13,7 @@ export function Header() {
     const isMobile = useMobile();
     return (
         <header className={cn("fixed top-0 left-0 w-full z-50 backdrop-blur-xs", scrolled && 'backdrop-blur-md bg-black/20')}>
-            <div className={cn("w-3/4 mx-auto border-b border-white flex flex-row justify-between items-center py-2", isMobile && "w-4/5")}>
+            <div className={cn("w-11/12 lg:w-3/4 mx-auto border-b border-white flex flex-row justify-between items-center py-3", isMobile && "w-4/5")}>
                 {/* Logo */}
                 <BluAzurLogo className="w-[109px] h-[48px]" />
 
