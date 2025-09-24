be1b5dc92340ca33e164d20e4629685e964d8bf2
Safidy RAMAROSON <safidyramaroson.patrick@gmail.com>
2025-09-24 14:20:08 +0300

    feat: Update SaintRaphaelServices layout, improve readability on mobile

--- components/organims/SaintRaphaelServices.tsx
+++ components/organims/SaintRaphaelServices.tsx
@@ -4,7 +4,7 @@ import { SaintRaphaelServiceItem } from "../molecules/SaintRaphaelServiceItem";
 
 export function SaintRaphaelServices() {
     return (
-        <div className="flex flex-row justify-center gap-4 space-y-5">
+        <div className="grid grid-cols-2 grid-rows-3 md:grid-cols-3 md:grid-rows-2 lg:grid-cols-6 lg:grid-rows-1 justify-center gap-4 space-y-5">
             {saintRaphaelServices.map((service) => (
                 <SaintRaphaelServiceItem
                     key={service.alt}
--- components/templates/SaintRaphaelSection.tsx
+++ components/templates/SaintRaphaelSection.tsx
@@ -6,12 +6,12 @@ export function SaintRaphaelSection() {
             <h2 className="text-[43px] text-primary-foreground font-normal text-center">
                 Saint Raphaël, l’endroit parfait pour <br /> <span className="font-bold">vivre la Côte d’Azur</span>
             </h2>
-            <p className="text-sm font-normal text-primary-foreground text-center pb-10">Avec ses plages dorées, ses 300 jours de soleil et ses calanques sauvages, Saint-Raphaël est une destination <br /> de rêve. Entre mer turquoise et rochers rouges de l’Estérel, elle offre un cadre idyllique pour la détente et <br /> l’aventure.</p>
+            <p className="text-sm font-normal text-primary-foreground text-center pb-10">Avec ses plages dorées, ses 300 jours de soleil et ses calanques sauvages, Saint-Raphaël est une destination <br className="max-md:hidden" /> de rêve. Entre mer turquoise et rochers rouges de l’Estérel, elle offre un cadre idyllique pour la détente et <br /> l’aventure.</p>
             <div className="w-5/6 mx-auto">
                 <SaintRaphaelServices />
             </div>
 
-            <p className="text-center text-primary-foreground text-sm py-10 font-normal">Flânez sur la Promenade des Bains, explorez les criques secrètes ou partez en mer depuis  son port de <br /> plaisance. Saint-Raphaël, c’est la Côte d’Azur dans toute sa splendeur !</p>
+            <p className="text-center text-primary-foreground text-sm py-10 font-normal">Flânez sur la Promenade des Bains, explorez les criques secrètes ou partez en mer depuis  son port de <br className="max-md:hidden" /> plaisance. Saint-Raphaël, c’est la Côte d’Azur dans toute sa splendeur !</p>
         </section>
     );
 }
