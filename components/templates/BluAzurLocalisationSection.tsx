31be3a6672d04a90064a9236248c69ebcfe20da2
Safidy RAMAROSON <safidyramaroson.patrick@gmail.com>
2025-09-25 09:52:40 +0300

    fix: update button text for location guide

--- components/templates/BluAzurLocalisationSection.tsx
+++ components/templates/BluAzurLocalisationSection.tsx
@@ -14,7 +14,7 @@ export function BluAzurLocalisationSection() {
                     <p className="text-[#020202] text-sm font-bold my-[21px]">Blu Azur est idéalement situé entre Saint-Tropez et Cannes, au cœur de l’une des plus belles stations balnéaires de France. Nos appartements se trouvent dans un domaine privé sécurisé, entouré de verdure, à quelques minutes à pied de la plage sablonneuse de Santa Lucia et de la marina. Proche du centre-ville de Saint-Raphaël, la résidence permet un accès facile aux boutiques, restaurants et animations locales. Une station d’arrêt d’autobus est située à la sortie du site, facilitant les déplacements, et une boulangerie à proximité vous offre du pain frais chaque matin pour bien commencer la journée.</p>
                     {/* how to get there */}
                     <Button size='lg' className="border border-primary text-primary hover:text-primary-foreground hover:border-primary-foreground cursor-pointer bg-white/80 rounded-full">
-                        Comment s&apos;y rendre
+                        Télécharger le PDF "Comment s&apos;y rendre ?"
                         <CircleArrowRight className="text-primary size-4 ml-2" />
                     </Button>
                 </div>
