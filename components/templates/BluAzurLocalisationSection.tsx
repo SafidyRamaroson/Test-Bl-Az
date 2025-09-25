a40394000f848e2c5aed2ac06675e675659a63c0
Safidy RAMAROSON <safidyramaroson.patrick@gmail.com>
2025-09-25 10:03:35 +0300

    feat: Link added for PDF download in BluAzurLocalisationSection

--- components/templates/BluAzurLocalisationSection.tsx
+++ components/templates/BluAzurLocalisationSection.tsx
@@ -1,6 +1,7 @@
 import { Button } from "../ui/button";
 import { CircleArrowRight } from "lucide-react";
 import { MapImage } from "../molecules/MapImage";
+import Link from "next/link";
 
 
 export function BluAzurLocalisationSection() {
@@ -11,12 +12,17 @@ export function BluAzurLocalisationSection() {
                 {/* Blu azur localisation */}
                 <div className="text-justify text-[#121212] mb-[21px]">
                     <h2 className="font-normal">Où se trouve Blu Azur à <br className="max-md:hidden" /> <span className="font-bold">Saint-Raphaël ?</span> </h2>
-                    <p className="text-[#020202] text-sm font-bold my-[21px]">Blu Azur est idéalement situé entre Saint-Tropez et Cannes, au cœur de l’une des plus belles stations balnéaires de France. Nos appartements se trouvent dans un domaine privé sécurisé, entouré de verdure, à quelques minutes à pied de la plage sablonneuse de Santa Lucia et de la marina. Proche du centre-ville de Saint-Raphaël, la résidence permet un accès facile aux boutiques, restaurants et animations locales. Une station d’arrêt d’autobus est située à la sortie du site, facilitant les déplacements, et une boulangerie à proximité vous offre du pain frais chaque matin pour bien commencer la journée.</p>
+                    <p className="text-[#020202] text-sm font-bold my-[21px]">Blu Azur est idéalement situé entre Saint-Tropez et Cannes, au cœur de l&apos;une des plus belles stations balnéaires de France. Nos appartements se trouvent dans un domaine privé sécurisé, entouré de verdure, à quelques minutes à pied de la plage sablonneuse de Santa Lucia et de la marina. Proche du centre-ville de Saint-Raphaël, la résidence permet un accès facile aux boutiques, restaurants et animations locales. Une station d&apos;arrêt d&apos;autobus est située à la sortie du site, facilitant les déplacements, et une boulangerie à proximité vous offre du pain frais chaque matin pour bien commencer la journée.</p>
                     {/* how to get there */}
-                    <Button size='lg' className="border border-primary text-primary hover:text-primary-foreground hover:border-primary-foreground cursor-pointer bg-white/80 rounded-full">
-                        Télécharger le PDF "Comment s&apos;y rendre ?"
-                        <CircleArrowRight className="text-primary size-4 ml-2" />
-                    </Button>
+                    <Link
+                        href='https://drive.google.com/file/d/1O197wWlKcDOsHp3eKCsa6918SI6N0H6X/view?usp=drive_link'
+                        target='_blank'
+                    >
+                        <Button size='lg' className="border border-primary text-primary hover:text-primary-foreground hover:border-primary-foreground cursor-pointer bg-white/80 rounded-full">
+                            Télécharger le PDF &quot;Comment s&apos;y rendre ?&quot;
+                            <CircleArrowRight className="text-primary size-4 ml-2" />
+                        </Button>
+                    </Link>
                 </div>
                 {/* Blu azur map */}
                 <MapImage />
